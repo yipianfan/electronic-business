44bb475dd9ae0eec17365161400b0740d1faa444
yipianfan <yipianfan@163.com>
2019-09-22 23:36:13 +0800

    common account-service 模块的初始化

---
+++ account-service/src/main/java/com/canoe/eb/account/controller/AccountController.java
@@ -0,0 +1,16 @@
+package com.canoe.eb.account.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+
+    @PostMapping("register")
+    public String register(@RequestParam String account, @RequestParam String password){
+        return "{account:" + account + ",password:" + password + "}";
+    }
+}
