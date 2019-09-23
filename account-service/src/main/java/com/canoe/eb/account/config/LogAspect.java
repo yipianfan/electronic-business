2067b74a28e7e361b795527ee1266018dbc83007
yipianfan <yipianfan@163.com>
2019-09-23 22:31:12 +0800

    日志AOP

---
+++ account-service/src/main/java/com/canoe/eb/account/config/LogAspect.java
@@ -0,0 +1,7 @@
+package com.canoe.eb.account.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogAspect {
+}
--- account-service/src/main/java/com/canoe/eb/account/controller/AccountController.java
+++ account-service/src/main/java/com/canoe/eb/account/controller/AccountController.java
@@ -1,21 +1,26 @@
 package com.canoe.eb.account.controller;
 
+import com.canoe.eb.common.entity.ResEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.canoe.eb.common.entity.ResEntity.ok;
 
 @RestController
 @RequestMapping("account")
 public class AccountController {
 
     @PostMapping("register")
-    public String register(@RequestParam String account, @RequestParam String password) {
-
-
-
-        return null;
-
-
+    public ResEntity<Map<String, String>> register(@RequestParam String account,
+                                                   @RequestParam String password) {
+        Map<String, String> map = new HashMap<>();
+        map.put("account", account);
+        map.put("password", password);
+        return ok(map);
     }
 }
