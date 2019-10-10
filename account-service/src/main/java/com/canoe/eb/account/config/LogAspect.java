26c71565d039466b74dd3726dd0814fc111c6e6d
yipianfan <yipianfan@163.com>
2019-10-10 09:00:09 +0800

    spring aop 依赖的加入

--- account-service/src/main/java/com/canoe/eb/account/config/LogAspect.java
+++ account-service/src/main/java/com/canoe/eb/account/config/LogAspect.java
@@ -1,7 +1,15 @@
 package com.canoe.eb.account.config;
 
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect
 public class LogAspect {
+
+    @Around("")
+    public Object aroundAdvice() {
+
+    }
 }
