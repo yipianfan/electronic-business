83ea0185d1d7696db741b3bcf3b87ef347b1f4b3
yipianfan <yipianfan@163.com>
2019-10-10 14:28:43 +0800

    添加fastjson依赖

--- account-service/src/main/java/com/canoe/eb/account/config/LogAspect.java
+++ account-service/src/main/java/com/canoe/eb/account/config/LogAspect.java
@@ -1,15 +1,29 @@
 package com.canoe.eb.account.config;
 
-import org.aspectj.lang.annotation.Around;
+import com.alibaba.fastjson.JSONObject;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Component
 @Aspect
 public class LogAspect {
 
-    @Around("")
-    public Object aroundAdvice() {
+    @Pointcut("execution(public * com.canoe.eb.account.controller.*.*(..))")
+    private void log() {}
 
+    @Before("log()")
+    public void doBefore() {
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = sra.getRequest();
+        Map<String, String[]> parameter = request.getParameterMap();
+
+        JSONObject.toJSONString(parameter);
     }
 }
